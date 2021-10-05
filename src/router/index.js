ae36d163992499c037d8acff8fb706af151f72d0
Mabelys Arias <MabelysA@gmail.com>
2021-10-05 14:43:12 -0400

    imports showsedit in index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from "../views/Home.vue";
 import ShowsIndex from "../views/ShowsIndex.vue";
 import ShowsNew from "../views/ShowsNew.vue";
 import ShowsShow from "../views/ShowsShow.vue";
+import ShowsEdit from "../views/PhotosEdit.vue";
 
 Vue.use(VueRouter);
 
@@ -28,6 +29,7 @@ const routes = [
   },
   { path: "/shows/new", name: "shows-new", component: ShowsNew },
   { path: "/shows/:id", name: "shows-show", component: ShowsShow },
+  { path: "/shows/:id/edit", name: "shows-edit", component: ShowsEdit },
 ];
 
 const router = new VueRouter({
