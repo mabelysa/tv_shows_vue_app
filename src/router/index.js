1ae5803a9029cff32ab2cd07f19ae72ae521ae8a
Mabelys Arias <MabelysA@gmail.com>
2021-10-05 13:11:54 -0400

    adds router link to app, imports showindex to index.js, creates showsindex page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import ShowsIndex from "../views/ShowsIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/shows",
+    name: "shows-index",
+    component: ShowsIndex,
   },
   {
     path: "/about",
