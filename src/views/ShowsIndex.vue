d7cbbeb29ab4d1563817e356474853b61b85e67a
Mabelys Arias <MabelysA@gmail.com>
2021-10-05 14:30:51 -0400

    adds router link to showsindex page

--- src/views/ShowsIndex.vue
+++ src/views/ShowsIndex.vue
@@ -3,7 +3,7 @@
     <h1>All Shows</h1>
     <div v-for="show in shows" v-bind:key="show.id">
       <h2>{{ show.title }}</h2>
-      <img v-bind:src="show.image" v-bind:alt="show.title" />
+      <img class="rounded img-thumbnail" v-bind:src="show.image" v-bind:alt="show.title" />
       <p>Year: {{ show.year }}</p>
       <p>Description: {{ show.description }}</p>
       <p>Creator: {{ show.creator }}</p>
@@ -11,6 +11,7 @@
       <p>Network: {{ show.network }}</p>
       <p>Favorite: {{ show.favorite }}</p>
       <p>User_id: {{ show.user_id }}</p>
+      <router-link v-bind:to="`/shows/${show.id}`">More Details</router-link>
     </div>
   </div>
 </template>
