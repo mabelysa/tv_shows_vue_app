0604787eb35e518f2594a9a420861a0afc0c347f
Mabelys Arias <MabelysA@gmail.com>
2021-10-05 14:23:31 -0400

    creates showsshow page

---
+++ src/views/ShowsShow.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="shows-show">
+    <h2>{{ show.title }}</h2>
+    <img v-bind:src="show.image" v-bind:alt="show.title" />
+    <p>Year: {{ show.year }}</p>
+    <p>Description: {{ show.description }}</p>
+    <p>Creator: {{ show.creator }}</p>
+    <p>Seasons: {{ show.seasons }}</p>
+    <p>Network: {{ show.network }}</p>
+    <p>Favorite: {{ show.favorite }}</p>
+    <p>Image: {{ show.image }}</p>
+    <p>User Id: {{ show.user_id }}</p>
+    <router-link to="/shows">Back to all shows</router-link>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      show: {},
+    };
+  },
+  created: function () {
+    axios.get("/shows/" + this.$route.params.id).then((response) => {
+      console.log("shows show", response);
+      this.show = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
